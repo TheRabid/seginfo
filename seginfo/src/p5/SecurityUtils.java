2b2d294e678a507fe226ecb13ccc9d8a6a01060b
Jaime <jai_mi_to@hotmail.es>
2015-12-03 09:01:01 +0100

    Que pasa ahora a ver

--- seginfo/src/p5/SecurityUtils.java
+++ seginfo/src/p5/SecurityUtils.java
@@ -2,6 +2,7 @@ package p5;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 import javax.crypto.KeyGenerator;
@@ -10,7 +11,23 @@ import javax.crypto.SecretKey;
 public class SecurityUtils {
 
 	public static String hashMsg(String msg){
-		
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		md.update(msg.getBytes());
+
+		byte byteData[] = md.digest();
+
+		// Convertir los bytes a hexadecimal
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < byteData.length; i++) {
+			sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+		}
+
+		return sb.toString();
 	}
 	
 	public static SecretKey generateSecretKey(int keyLength){
@@ -19,7 +36,6 @@ public class SecurityUtils {
 			KeyGenerator g = KeyGenerator.getInstance("RSA");
 			s = g.generateKey();
 		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return s;
