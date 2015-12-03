805e712171c0a76bfd0a2615a8c04a4bfa7180ca
Jaime <jai_mi_to@hotmail.es>
2015-12-03 09:10:03 +0100

    Ñam ñam conflicts!

--- seginfo/src/p5/Main.java
+++ seginfo/src/p5/Main.java
@@ -9,19 +9,30 @@ public class Main {
 	final private static int KEY_LENGTH = 100;
 	final private static String ALGORITMO = "RSA";
 	final private static String MENSAJE = "VIVA PIT";
-	
+
 	public static void main(String[] args) {
-		
+
 		/* Hash del mensaje */
-		String hash = SecurityUtils.hashMsg(MENSAJE);
-		
-		/* Criptografia de clave publica */
+		long startTime = System.nanoTime();
+		String hash = SecurityUtils.hashMsg(MENSAJE, ALGORITMO);
+		long endTime = System.nanoTime();
+		long duration = (endTime - startTime) / (long) (1000000.0);
+		System.out.println("Tiempo de ejecución de hasheo: " + duration + " milisegundos");
+
+		/* Criptografia de clave secreta */
+		startTime = System.nanoTime();
 		KeyPair keyPair = SecurityUtils.generatePrivatePublicKey(KEY_LENGTH, ALGORITMO);
-		
+		endTime = System.nanoTime();
+		duration = (endTime - startTime) / (long) (1000000.0);
+		System.out.println("Tiempo de ejecución de generación de clave secreta: " + duration + " milisegundos");
+
 		/* Criptografia de clave publica */
+		startTime = System.nanoTime();
 		SecretKey secretKey = SecurityUtils.generateSecretKey(KEY_LENGTH, ALGORITMO);
-		
+		endTime = System.nanoTime();
+		duration = (endTime - startTime) / (long) (1000000.0);
+		System.out.println("Tiempo de ejecución de generación de clave secreta: " + duration + " milisegundos");
 		/* Firma digital */
-		
+
 	}
 }
