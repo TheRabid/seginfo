b71c98358c9f8879b92e8844a12804da9672810f
Jaime <jai_mi_to@hotmail.es>
2015-12-12 15:07:05 +0100

    Updateado con KeyStore. Aunque no sé como hacerlo para clave privada

--- seginfo/src/p5/Main.java
+++ seginfo/src/p5/Main.java
@@ -1,24 +1,32 @@
 package p5;
 
+import java.io.IOException;
 import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.Signature;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 import javax.crypto.SecretKey;
 
 /**
  * 
- * @author Alejandro Royo Amondarain (NIP: 560285)
- * 			Jaime Ruiz-Borau Vizárraga (NIP: 546751)
+ * @author Alejandro Royo Amondarain (NIP: 560285) Jaime Ruiz-Borau Vizárraga
+ *         (NIP: 546751)
  *
- *	Esta clase contiene el codigo correspondiente a las pruebas y medida de
- *	tiempos de los diferentes metodos de hash, encriptacion y firma digital
- *	solicitados en el guion de la practica 5 de Seguridad Informatica.
+ *         Esta clase contiene el codigo correspondiente a las pruebas y medida
+ *         de tiempos de los diferentes metodos de hash, encriptacion y firma
+ *         digital solicitados en el guion de la practica 5 de Seguridad
+ *         Informatica.
  */
 public class Main {
 
 	final private static int[] KEY_LENGTHS = { 56, 512 };
-	final private static String[] ALGORITMOS = { "SHA-256", "DES", "RSA","SHA256withRSA" };
+	final private static String[] ALGORITMOS = { "SHA-256", "DES", "RSA", "SHA256withRSA" };
 	final private static String MENSAJE = "VIVA PIT";
+	final private static String PASSWORD = "VIVAPODEMOS";
 
 	public static void main(String[] args) {
 
@@ -31,6 +39,25 @@ public class Main {
 		long duration = (endTime - startTime) / (long) (1000000.0);
 		System.out.println("Tiempo de ejecución de hasheo: " + duration + " milisegundos");
 
+		/* Creacion del almacen de claves */
+		KeyStore ks = null;
+		try {
+			ks = KeyStore.getInstance(KeyStore.getDefaultType());
+			ks.load(null, PASSWORD.toCharArray());
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+			System.exit(0);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			System.exit(0);
+		} catch (CertificateException e) {
+			e.printStackTrace();
+			System.exit(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+
 		/* Criptografia de clave secreta */
 		System.out.println("Comienzo de generación de clave secreta");
 		System.out.println("Generando...");
@@ -39,6 +66,13 @@ public class Main {
 		endTime = System.nanoTime();
 		duration = (endTime - startTime) / (long) (1000000.0);
 		System.out.println("Tiempo de ejecución de generación de clave secreta: " + duration + " milisegundos");
+		// Almacenar clave secreta
+		KeyStore.SecretKeyEntry skEntry = new KeyStore.SecretKeyEntry(secretKey);
+		try {
+			ks.setEntry("claveSecreta", skEntry, new KeyStore.PasswordProtection(PASSWORD.toCharArray()));
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+		}
 
 		/* Criptografia de clave publica */
 		System.out.println("Comienzo de generación de clave privada y clave pública");
@@ -49,6 +83,13 @@ public class Main {
 		duration = (endTime - startTime) / (long) (1000000.0);
 		System.out
 				.println("Tiempo de ejecución de generación de clave publica y privada: " + duration + " milisegundos");
+		// Almacenar clave secreta
+		KeyStore.PrivateKeyEntry pkEntry = new KeyStore.PrivateKeyEntry(keyPair.getPrivate(),);
+		try {
+			ks.setEntry("clavePrivada", pkEntry, new KeyStore.PasswordProtection(PASSWORD.toCharArray()));
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+		}
 
 		/* Firma digital */
 		System.out.println("Comienzo de creación de firma digital");
@@ -57,8 +98,7 @@ public class Main {
 		Signature firma = SecurityUtils.createDigitalSignature(MENSAJE, keyPair, ALGORITMOS[0], ALGORITMOS[3]);
 		endTime = System.nanoTime();
 		duration = (endTime - startTime) / (long) (1000000.0);
-		System.out
-				.println("Tiempo de ejecución de creación de firma digital: " + duration + " milisegundos");
-		
+		System.out.println("Tiempo de ejecución de creación de firma digital: " + duration + " milisegundos");
+
 	}
 }
