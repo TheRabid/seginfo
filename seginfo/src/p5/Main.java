781799760bd26608c869f0a4832794995aa3b1fb
Jaime <jai_mi_to@hotmail.es>
2015-12-14 19:35:49 +0100

    VAS A FLIPAR FELINO

--- seginfo/src/p5/Main.java
+++ seginfo/src/p5/Main.java
@@ -1,13 +1,28 @@
 package p5;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
 import java.security.Signature;
+import java.security.SignatureException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.bouncycastle.jce.X509Principal;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.x509.X509V3CertificateGenerator;
 
 import javax.crypto.SecretKey;
 
@@ -21,6 +36,7 @@ import javax.crypto.SecretKey;
  *         digital solicitados en el guion de la practica 5 de Seguridad
  *         Informatica.
  */
+@SuppressWarnings({ "deprecation", "unused" })
 public class Main {
 
 	final private static int[] KEY_LENGTHS = { 56, 512 };
@@ -28,7 +44,8 @@ public class Main {
 	final private static String MENSAJE = "VIVA PIT";
 	final private static String PASSWORD = "VIVAPODEMOS";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws KeyStoreException, InvalidKeyException, IllegalStateException,
+			NoSuchProviderException, NoSuchAlgorithmException, SignatureException, CertificateException, IOException {
 
 		/* Hash del mensaje */
 		System.out.println("Comienzo del hasheo");
@@ -42,7 +59,7 @@ public class Main {
 		/* Creacion del almacen de claves */
 		KeyStore ks = null;
 		try {
-			ks = KeyStore.getInstance(KeyStore.getDefaultType());
+			ks = KeyStore.getInstance("JCEKS");
 			ks.load(null, PASSWORD.toCharArray());
 		} catch (KeyStoreException e) {
 			e.printStackTrace();
@@ -84,12 +101,12 @@ public class Main {
 		System.out
 				.println("Tiempo de ejecución de generación de clave publica y privada: " + duration + " milisegundos");
 		// Almacenar clave secreta
-		KeyStore.PrivateKeyEntry pkEntry = new KeyStore.PrivateKeyEntry(keyPair.getPrivate(),);
-		try {
-			ks.setEntry("clavePrivada", pkEntry, new KeyStore.PasswordProtection(PASSWORD.toCharArray()));
-		} catch (KeyStoreException e) {
-			e.printStackTrace();
-		}
+		X509Certificate certificate = generateCertificate(keyPair, ALGORITMOS[3]);
+		KeyStore keyStore = KeyStore.getInstance("JKS");
+		keyStore.load(null, null);
+		Certificate[] certChain = new Certificate[1];
+		certChain[0] = certificate;
+		keyStore.setKeyEntry("privatekey", (Key) keyPair.getPrivate(), PASSWORD.toCharArray(), certChain);
 
 		/* Firma digital */
 		System.out.println("Comienzo de creación de firma digital");
@@ -100,5 +117,27 @@ public class Main {
 		duration = (endTime - startTime) / (long) (1000000.0);
 		System.out.println("Tiempo de ejecución de creación de firma digital: " + duration + " milisegundos");
 
+	}
+
+	public static X509Certificate generateCertificate(KeyPair keyPair, String alg)
+			throws CertificateEncodingException, InvalidKeyException, IllegalStateException, NoSuchProviderException,
+			NoSuchAlgorithmException, SignatureException {
+		X509V3CertificateGenerator cert = new X509V3CertificateGenerator();
+		cert.setSerialNumber(BigInteger.valueOf(1)); // or generate a random
+														// number
+		cert.setSubjectDN(new X509Principal("CN=localhost")); // see examples to
+																// add O,OU etc
+		cert.setIssuerDN(new X509Principal("CN=localhost")); // same since it is
+																// self-signed
+		cert.setPublicKey(keyPair.getPublic());
+		Calendar c1 = new GregorianCalendar();
+		c1.set(2015, 12, 14);
+		Calendar c2 = new GregorianCalendar();
+		c2.set(2016, 12, 14);
+		cert.setNotBefore(c1.getTime());
+		cert.setNotAfter(c2.getTime());
+		cert.setSignatureAlgorithm(alg);
+		PrivateKey signingKey = keyPair.getPrivate();
+		return cert.generate(signingKey);
 	}
 }
